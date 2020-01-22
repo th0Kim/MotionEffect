5509f4ecdd3c9d39983fe6cc0bdaf07c9c0a3bb8
soyoung <sykim58@m4nc.co.kr>
2020-01-22 16:41:38 +0900

    fullpage 0.0.1

--- sampleEffect/js/fullpage_ui.js
+++ sampleEffect/js/fullpage_ui.js
@@ -1,8 +1,11 @@
+/*
+// ver 0.0.1
+*/
 function hasJqueryObject($elem) { return $elem.length > 0 }
 var app = app || {};
 
 app = {
-  mainSlide: function(fullpage) {
+  mainSlide: function(fullpage, swiperPager) {
     var swiper = new Swiper(fullpage, {
       direction: 'vertical',
       slidesPerView: 1,
@@ -11,11 +14,10 @@ app = {
         forceToAxis: true,
         invert: true,
       },
-      preventInteractionOnTransition: true,
       speed: 1000,
       parallax: true,
       pagination: {
-        el: '.swiper-pagination',
+        el: swiperPager,
         clickable: true,
       },
       on: {
@@ -25,59 +27,34 @@ app = {
           slideChange: function() {
             var idx = swiper.activeIndex;
             var checkID = $(fullpage).find('.swiper-slide').eq(idx).attr('id');
-            app.addEvent(checkID)
-            // console.log('슬라이드체인지')
+            app.addEvent(checkID);
           }
       },
-      // preventInteractionOnTransition: true,
-      // simulateTouch: simulatetouch,
-      // allowTouchMove: allowtouchmove,
-      // followFinger: followfinger,
     });
   },
   addEvent: function(id) {
     var $id = '#'+ id;
-    // var WINDOWLOAD = null;
-    var FIRST_ANIMATE,
-        SECOND_ANIMATE,
-        THIRD_ANIMATE,
-        FOURTH_ANIMATE = null;
+    var animate = '.animate';
+    // TweenMax.set($('.animate'), {opacity:0, scale: 3}); // active 때 마다 모션을 주고 싶으면 set값 설정 할 것
     var handler = {
       FIRST: function FIRST(){
-        if(FIRST_ANIMATE) return; // 한번만 실행
-        FIRST_ANIMATE = true; // 한번만 실행
-
-        var _this = $($id);
-        var animate = _this.find('.animate');
-        var aniLength = animate.length;
-        TweenMax.to(animate, 1, {opacity:1, scale: 2});
+        var $animate = $($id).find(animate);
+        TweenMax.to($animate, 1, {opacity:1, scale: 2});
       },
       SECOND: function SECOND(){
-        if(SECOND_ANIMATE) return;
-        SECOND_ANIMATE = true;
-
         var _this = $($id);
-        var animate = _this.find('.animate');
-        var aniLength = animate.length;
-        TweenMax.to(animate, 1, {opacity:1, scale: 2});
+        var $animate = _this.find(animate);
+        TweenMax.to($animate, 1, {opacity:1, scale: 2});
       },
       THIRD: function THIRD(){
-        if(THIRD_ANIMATE) return;
-        THIRD_ANIMATE = true;
-        
         var _this = $($id);
-        var animate = _this.find('.animate');
-        var aniLength = animate.length;
-        TweenMax.to(animate, 1, {opacity:1, scale: 2});
+        var $animate = _this.find(animate);
+        TweenMax.to($animate, 1, {opacity:1, scale: 2});
       },
       FOURTH: function FOURTH(){
-        if(FOURTH_ANIMATE) return;
-        FOURTH_ANIMATE = true;
-
         var _this = $($id);
-        var animate = _this.find('.animate');
-        var aniLength = animate.length;
-        TweenMax.to(animate, 1, {opacity:1, scale: 2});
+        var $animate = _this.find(animate);
+        TweenMax.to($animate, 1, {opacity:1, scale: 2});
       },
     }
     handler[id]();
@@ -86,5 +63,6 @@ app = {
   
 $(function(){
   app.$body = $("body");
-	hasJqueryObject(app.$body.find(".fullpage")) && app.mainSlide(".fullpage");
+	hasJqueryObject(app.$body.find(".fullpage")) && app.mainSlide(".fullpage", ".swiper-pagination");
+
 })
