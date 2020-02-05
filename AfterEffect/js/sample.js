781951899c9126651aa0aebb05c68dfe63e7638d
soyoung <sykim58@m4nc.co.kr>
2020-02-05 10:47:01 +0900

    이벤트별 json 파일 play/stop

--- AfterEffect/js/sample.js
+++ AfterEffect/js/sample.js
@@ -4,35 +4,44 @@ function btnClick01(id,rep){
     renderer: 'svg',
     loop: rep,
     autoplay: false,
-    path: 'https://assets2.lottiefiles.com/packages/lf20_wX69nA.json'
+    path: 'https://assets2.lottiefiles.com/packages/lf20_wX69nA.json' //json파일이 있을 시 path: '경로'+ id + '.json'
   }
   return bodymovin.loadAnimation(options);
 }
 
-var animation = btnClick01('check', 1);
-var animation2 = btnClick01('heart', true);
-var animation3 = btnClick01('circle', 2);
+var animation = btnClick01('first', 1);
+var animation2 = btnClick01('second', 1);
+var animation3 = btnClick01('thirth', true);
+var animation4 = btnClick01('fourth', true);
 
   $(function(){
-    $('.btn03').on('click', function(){
+    //오버하면 first 움직이 1번 시작돼요~
+    $('.btn01').on('mouseenter', function(){
       animation.play();
-    });
+    })
+    //클릭하면 second 1번 움직여요~
     $('.btn02').on('click', function(){
-      animation2.play();
+      $(this).toggleClass('on');
+      if( $(this).hasClass('on') ){
+        animation2.play();
+      } else {
+        animation2.pause();
+      }
     });
+    //오버하면 thirth 무한 움직여요~ 벗어나면 멈춰요!
+    $('.btn03').on('mouseenter', function(){
+      animation3.play();
+    }).on('mouseleave', function(){
+      animation3.pause();
+    });
+    //포커스 되면 thirth 무한 움직임이 시작돼요~
     $('.input01').on('focus', function(){
       animation3.play();
     });
-    $('.btn01').on('mouseenter', function(){
-      animation.play();
-    });
-    $('.btn01').on('mouseleave', function(){
-      animation.pause();
-    });
-    $('.btn02').on('mouseenter', function(){
-      animation2.play();
-    });
-    $('.btn02').on('mouseleave', function(){
-      animation2.pause();
-    });
+    // /포커스 되면 fourth 무한 움직임이 시작돼요~ 해제되면 멈춰요!
+    $('.input02').on('focus', function(){
+      animation4.play();
+    }).on('focusout', function(){
+      animation4.pause();
+    })
   });
